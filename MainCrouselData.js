2cef9398ab4bebee06106b79ce298beefb20b883
ansh007sharma <101504709+ansh007sharma@users.noreply.github.com>
2025-12-01 20:58:45 +0530

    Create MainCrouselData.js

---
+++ MainCrouselData.js
@@ -0,0 +1,19 @@
+export const mainCarouselData = [
+  {
+    image:
+      "https://i.pinimg.com/1200x/99/1f/32/991f32b56e6d1c2a17e9f8128f8fe8c9.jpg",
+  },
+
+  {
+    image:
+      "https://i.pinimg.com/1200x/8c/ab/6c/8cab6c7580f02f4ee10c9658a2939365.jpg",
+  },
+  {
+    image:
+      "https://i.pinimg.com/1200x/12/11/f5/1211f562dcefe5a5ea3f58d9e7e2d98d.jpg",
+  },
+  {
+    image:
+      "https://i.pinimg.com/1200x/66/88/5e/66885ef35acf816d3c0de96b04c5081b.jpg",
+  },
+];
