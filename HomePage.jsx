31ef05d055c133c4543db3a21ea0a9841d289f89
ansh007sharma <101504709+ansh007sharma@users.noreply.github.com>
2025-12-01 21:00:13 +0530

    Create HomePage.jsx

---
+++ HomePage.jsx
@@ -0,0 +1,11 @@
+import MainCarousel from "../../components/HomeCarousel/MainCarousel"
+
+const HomePage = () => {
+  return (
+    <div><MainCarousel />
+    <div>other sections</div>
+    </div>
+  
+  )
+}
+export default HomePage
