5ab8e52e5a3cea703a69a8046f9eabc6a42a5b47
ansh007sharma <101504709+ansh007sharma@users.noreply.github.com>
2025-12-01 20:59:33 +0530

    Add HomeSectionCard component

---
+++ Home/HomeSection.jsx
@@ -0,0 +1,10 @@
+const HomeSectionCard = () => {
+  return (
+    <div className='cursor-pointer flex flec-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem]mx-3'>
+      <div className='h-[13rem] w-[10rem]'>
+        <img src="" alt=""></img>
+      </div>
+      </div>
+  )
+}
+export default HomeSectionCard
